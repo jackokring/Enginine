4180bb6f9e57ab991fb84420d918b10382181a0c
Simon Jackson <jackokring@gmail.com>
2026-05-22 13:42:52 +0000

    1st

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,49 @@
+/*==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+EnginineAudioProcessorEditor::EnginineAudioProcessorEditor (EnginineAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+    addAndMakeVisible (volumeSlider);
+    volumeSlider.setTextValueSuffix (" Louds");
+    volumeSlider.onValueChange = [this] {
+      *audioProcessor.volume = volumeSlider.getValue();
+    };
+    volumePA = new juce::SliderParameterAttachment(*audioProcessor.volume, volumeSlider);
+    addAndMakeVisible (volumeLabel);
+    volumeLabel.setText ("Volume", juce::dontSendNotification);
+    volumeLabel.attachToComponent (&volumeSlider, true);
+}
+
+EnginineAudioProcessorEditor::~EnginineAudioProcessorEditor()
+{
+    delete volumePA;
+}
+
+//==============================================================================
+void EnginineAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (15.0f));
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void EnginineAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+    volumeSlider.setBounds (20, 20, 40, 40);
+}
---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,200 @@
+/*==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+EnginineAudioProcessor::EnginineAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    addParameter (
+        volume = new juce::AudioParameterFloat (
+            "gain", // parameter ID
+            "Gain", // parameter name
+            juce::NormalisableRange<float> (0.0f, 1.0f), // parameter range
+            0.5f)); // default value
+}
+
+EnginineAudioProcessor::~EnginineAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String EnginineAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool EnginineAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool EnginineAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool EnginineAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double EnginineAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int EnginineAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int EnginineAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void EnginineAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String EnginineAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void EnginineAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void EnginineAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+
+    previousVolume = *volume;
+}
+
+void EnginineAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool EnginineAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void EnginineAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    {
+        buffer.applyGainRamp (0, buffer.getNumSamples(), previousVolume, *volume);
+        previousVolume = *volume;
+    }
+}
+
+//==============================================================================
+bool EnginineAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* EnginineAudioProcessor::createEditor()
+{
+    return new EnginineAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void EnginineAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    std::unique_ptr<juce::XmlElement> xml (new juce::XmlElement ("EnginineState"));
+    xml->setAttribute ("volume", (double) *volume);
+    copyXmlToBinary (*xml, destData);
+}
+
+void EnginineAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+    if (xmlState.get() != nullptr) {
+        if (xmlState->hasTagName ("EnginineState")) {
+            *volume = (float) xmlState->getDoubleAttribute ("volume", 1.0);
+        }
+    }
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new EnginineAudioProcessor();
+}
